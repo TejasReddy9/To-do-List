9286f26bb77412b28744dc5aea8810d4e5491cee
Tejas Reddy <reddy.2@iitj.ac.in>
2017-06-11 10:43:15 +0530

    Main node.js files

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var todoController = require(__dirname + '/public/js/todoController');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8000);
+console.log('Listening to port 8000');
+
+todoController(app);
